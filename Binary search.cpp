f4c166ebcfb5725405df469b5656654331e1e25b
Rifal04 <116936225+Rifal04@users.noreply.github.com>
2022-12-23 12:54:33 +0700

    Add files via upload

---
+++ Binary search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    const int arraySize=5;
+    int target;
+    int array[arraySize]={1,2,3,4,5};
+    int first, mid, last;
+
+    cout << "••••RIFAL FAJAR SIDIQ•••••" << endl << endl;
+    cout << "Masukan angka yang dicari: ";cin >> target;
+    first=0; //Initialize first and last variables.
+    last=2;
+
+    while(first<=last)
+    {
+        mid=(first+last)/2;
+        if(target>array[mid])
+        {
+            first=mid+1;
+        }else if(target<array[mid])
+        {
+            last=mid+1;
+        }else{
+            first=last+1;
+        }
+    }
+    if(target==array[mid])
+    {
+        cout<<"Angka ditemukan."<<endl;
+    }else{
+        cout<<"Angka tidak ditemukan."<<endl;
+    }
+}
